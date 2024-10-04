90c65c89b798c2a41ce89e2f7978cee51a53a0e8
NyLaurent <laulanyumbayire@gmail.com>
2024-10-04 11:49:04 -0600

    finish

--- components/NavBar/nav.tsx
+++ components/NavBar/nav.tsx
@@ -3,25 +3,25 @@ import Link from 'next/link'; // Make sure to import Link from 'next/link'
 export default function Navigation() {
   return (
     <div className="flex flex-row gap-8 pt-3 justify-center">
-      <Link href="/" className="relative group text-gray-700 hover:text-blue-600">
+      <Link href="/" className="relative group text-gray-700 opacity-90 hover:text-primary">
         Home
-        <span className="absolute left-0 right-0 bottom-0 h-1 bg-blue-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+        <span className="absolute left-0 right-0 bottom-0 h-1 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Link>
-      <Link href="/" className="relative group text-gray-700 hover:text-blue-600">
+      <Link href="/" className="relative group text-gray-700 opacity-90 hover:text-primary">
         Best Seller
-        <span className="absolute left-0 right-0 bottom-0 h-1 bg-blue-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+        <span className="absolute left-0 right-0 bottom-0 h-1 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Link>
-      <Link href="/" className="relative group text-gray-700 hover:text-blue-600">
+      <Link href="/" className="relative group text-gray-700 opacity-90 hover:text-primary">
         Category
-        <span className="absolute left-0 right-0 bottom-0 h-1 bg-blue-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+        <span className="absolute left-0 right-0 bottom-0 h-1 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Link>
-      <Link href="/" className="relative group text-gray-700 hover:text-blue-600">
+      <Link href="/" className="relative group text-gray-700 opacity-90 hover:text-primary">
         Community
-        <span className="absolute left-0 right-0 bottom-0 h-1 bg-blue-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+        <span className="absolute left-0 right-0 bottom-0 h-1 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Link>
-      <Link href="/" className="relative group text-gray-700 hover:text-blue-600">
+      <Link href="/" className="relative group text-gray-700 opacity-90 hover:text-primary">
         Blog
-        <span className="absolute left-0 right-0 bottom-0 h-1 bg-blue-600 scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
+        <span className="absolute left-0 right-0 bottom-0 h-1 bg-primary scale-x-0 group-hover:scale-x-100 transition-transform duration-300" />
       </Link>
     </div>
   );
